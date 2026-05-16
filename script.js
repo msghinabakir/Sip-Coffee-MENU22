bd8c187febfe715663f1be4b2909a94ce75346e0
msghinabakir <msghinabakir@gmail.com>
2026-05-16 12:35:23 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+/* =======================================COFFEE MODAL ===========================================*/
+function openModal(id){
+  document.getElementById(id).style.display = "flex";
+}
+
+function closeModal(id){
+  document.getElementById(id).style.display = "none";
+}
